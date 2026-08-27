3641327adb83e5432e141bd8422f0e45d6aeb55f
Takumi Hendricks <takumi@canopyteam.org>
2026-08-27 10:22:50 -0500

    Update wtm to 0.9.1
    Point the cask at the verified macOS arm64 artifact from v0.9.1.
    
    SHA-256: ad4f060f7510169a3b42d816c623b3559ebd9a9ed8ae2d5795a75bd0def5f92b

--- Casks/wtm.rb
+++ Casks/wtm.rb
@@ -1,6 +1,6 @@
 cask "wtm" do
-  version "0.9.0"
-  sha256 "9c70405513b972dae0bb4c5ce2ed19680a162aaf27b11e4c15b3160f3782b115"
+  version "0.9.1"
+  sha256 "ad4f060f7510169a3b42d816c623b3559ebd9a9ed8ae2d5795a75bd0def5f92b"
 
   url "https://github.com/TakumiHendricksDev/worktreemanager/releases/download/v#{version}/wtm-#{version}-macos-arm64.zip"
   name "Worktree Manager"
